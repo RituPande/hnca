6a4dc1c2e3dd6ba49f93c6bacc04df0fbdd4d2a4
ritupande <ritz.pande@gmail.com>
2022-07-23 18:44:36 -0400

    scale RBG image by 255 on reading

--- framework/utils.py
+++ framework/utils.py
@@ -6,18 +6,26 @@ import PIL
 def load_image( image_path, max_size = 224):  
   img =  PIL.Image.open(image_path)
   img.thumbnail((max_size, max_size), PIL.Image.ANTIALIAS)
-  img = np.float32(img)
+  img = np.float32(img)/255.0
   return img
 
 def show_image( img ):
-  img = PIL.Image.fromarray(np.uint8(img), 'RGB' ) 
+  img = PIL.Image.fromarray(np.uint8(img) , mode='RGB') 
   img.show()
 
 def to_rgba(x):
   return x[..., :4]
 
+#def to_alpha(x):
+#  return tf.clip_by_value(x[..., 3:4], 0.0, 1.0)
+
 def to_rgb(x):
   return x[..., :3]
+  # assume rgb premultiplied by alpha
+  #rgb, a = x[..., :3], to_alpha(x)
+  #return 1.0-a+rgb
+
+  
 
 def plot_loss(loss_log):
   plt.figure(figsize=(10, 4))
