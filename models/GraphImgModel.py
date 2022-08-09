5f608f43eae51894f3950729ebb73b1faa8b8bc2
Ritu Pande <ritz.pande@gmail.com>
2022-08-09 18:22:13 +0000

    adding overflow loss

--- models/GraphImgModel.py
+++ models/GraphImgModel.py
@@ -62,11 +62,12 @@ class GraphImgModel(Model):
         with tf.GradientTape() as t:
             for i in range(step_n):
                 x = self(x)
-            loss = self.leaf_ca_loss(tf.identity(x))
+            #overflow loss forces the model to output values within -1.0 and 1.0
+            overflow_loss = tf.reduce_sum(tf.abs(x - tf.clip_by_value(x, -1.0, 1.0)))
+            loss = self.leaf_ca_loss(tf.identity(x)) + overflow_loss*1e3
 
-        # dont store outputs of initial few epochs as their outputs might not be stable.
         if use_pool :
-            self.replay_buffer.add(x.numpy())
+          self.replay_buffer.add(x.numpy())
                        
         variables = self.trainable_variables
         grads = t.gradient(loss, variables)
