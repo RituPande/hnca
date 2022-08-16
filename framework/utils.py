672f96db9b90f0a3ba440bb3c7a2b5e8354c5bd2
ritupande <ritz.pande@gmail.com>
2022-08-15 21:21:51 -0400

    adding code for cell detector

--- framework/utils.py
+++ framework/utils.py
@@ -10,6 +10,65 @@ from scipy import ndimage
 from skimage.filters import threshold_otsu
 from skimage.color import rgb2gray
 
+
+class CellDetector:
+
+  def __init__(self, min_pixels, max_pixels):
+    self.min_pixels = min_pixels
+    self.max_pixels = max_pixels
+
+  def __call__(self, img ):
+    img_mask = self._preprocess_img( img )
+    cell_masks, cell_members = self._extract_cells( img_mask )
+    return cell_masks, cell_members
+
+
+  def _preprocess_img(self, img):
+    img_gray = rgb2gray(img)
+    thresh_val = threshold_otsu(img_gray)
+    img_mask = np.where(img_gray > thresh_val, 1, 0)
+    
+    labels, _ = ndimage.label(img_mask)
+    # find connected objects.
+    for label_ind, label_coords in enumerate(ndimage.find_objects(labels)):
+      cell = img_gray[label_coords]
+      # Check if the label size is too small
+      if np.product(cell.shape) < self.min_pixels or np.product(cell.shape) > self.max_pixels: 
+        #print('Label {} is too small! Setting to 0.'.format(label_ind))
+        img_mask = np.where(labels==label_ind+1, 0, img_mask)
+
+    return img_mask
+
+  def _fill_cell( self, pix):
+    cell = {}
+
+    y_min =  min(pix[0])
+    y_max = max(pix[0])
+    x_min =  min(pix[1])
+    x_max = max(pix[1])
+    center_x = (x_min + x_max)//2
+    center_y = (y_min + y_max)//2
+   
+    cell['center'] = (center_y, center_x)
+    cell['members'] = np.asarray(pix).T
+    return cell
+
+  def _extract_cells(self, img_mask ):
+
+    # Generate the labels
+    labels, nlabels = ndimage.label(img_mask)
+    cell_masks = []
+    cell_members = []
+    for label_num in range(1, nlabels+1):
+      mask = np.where(labels == label_num, 1, 0)
+      cell_masks.append(mask)
+      pix = np.where(labels == label_num )
+      cell = self._fill_cell(pix)
+      cell_members.append(cell)
+
+    return cell_masks, cell_members
+
+    
 class ReplayBuffer:
   def __init__(self, max_len=256 ):
     self.buffer = deque(maxlen=max_len) 
@@ -21,8 +80,6 @@ class ReplayBuffer:
     mini_batch = random.sample( self.buffer, min(len(self.buffer), batch_size))
     mini_batch = np.asarray(mini_batch)
     return mini_batch
-
-
 
 def load_image( image_path, max_size = 128):  
   img =  PIL.Image.open(image_path)
@@ -44,9 +101,7 @@ def to_rgba(x):
 def to_rgb(x):
   return x[..., :3]+ 0.5
 
-  # assume rgb premultiplied by alpha
-  #rgb, a = x[..., :3], to_alpha(x)
-  #return 1.0-a+rgb
+
 def plot_loss(loss_log):
   plt.figure(figsize=(10, 4))
   plt.title('Loss history ')
@@ -54,30 +109,6 @@ def plot_loss(loss_log):
   plt.plot(loss_log,"o" , alpha=0.5)
   plt.show()
 
-def find_cells( img ):
-  img_gray = rgb2gray(img)
+
     
-  # Mask out background and extract connected objects
-  thresh_val = threshold_otsu(img_gray)
-  mask = np.where(img_gray > thresh_val, 1, 0)
-  
-  labels, nlabels = ndimage.label(mask)
-
-  # find connected objects.
-  for label_ind, label_coords in enumerate(ndimage.find_objects(labels)):
-    cell = img_gray[label_coords]
-    # Check if the label size is too small
-    if np.product(cell.shape) < 10: 
-      print('Label {} is too small! Setting to 0.'.format(label_ind))
-      mask = np.where(labels==label_ind+1, 0, mask)
-
-  # create mask for each component 
-  cell_masks = []
-  for label_num in range(1, nlabels+1):
-    mask = np.where(labels == label_num, 1, 0)
-    cell_masks.append(mask)
-
-  # Regenerate the labels
-  labels, nlabels = ndimage.label(mask)
-  print('There are now {} separate components / objects detected.'.format(nlabels))
     
