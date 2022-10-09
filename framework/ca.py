27debb3de5a9a5ab17f8c12539663274f59d310d
ritupande <ritz.pande@gmail.com>
2022-10-08 22:15:44 -0400

    hca code complete

---
+++ framework/ca.py
@@ -0,0 +1,118 @@
+
+import tensorflow as tf
+from tensorflow import keras
+from  keras.layers import  Conv2D, DepthwiseConv2D,Input,Concatenate
+from keras.models import Model
+import numpy as np
+
+
+
+class ImgCA(Model):
+
+    """
+    This is a derived class to create a leaf CA that learns rules to create a target image 
+    
+    Attributes
+    ----------
+    
+    Methods
+    -------
+
+    """
+
+    class PerceptionKernelInitializer(tf.keras.initializers.Initializer):
+
+        def __call__(self, shape, dtype=None):
+            ident = tf.constant([[0.0,0.0,0.0],[0.0,1.0,0.0],[0.0,0.0,0.0]])
+            sobel_x = tf.constant([[-1.0,0.0,1.0],[-2.0,0.0,2.0],[-1.0,0.0,1.0]])
+            sobel_y = tf.constant([[-1.0,-2.0,-1.0],[0.0,0.0,0.0],[1.0,2.0,1.0]])
+            lap = tf.constant([[1.0,2.0,1.0],[2.0,-12,2.0],[1.0,2.0,1.0]])
+
+            kernel = tf.stack([ident, sobel_x, sobel_y, lap], axis=-1)[:,:,None,:]
+            kernel = tf.repeat(kernel, self.n_channels + self.n_schannels , axis=2)
+            return kernel
+
+    def __init__(self, n_channels, n_schannels ):
+
+        super().__init__()
+
+        self._init_ca_class_members(n_channels, n_schannels )
+        self._init_ca_layers()
+        
+   
+    def _init_ca_class_members(self, n_channels, n_schannels):
+        
+        self.n_channels = n_channels
+        self.n_schannels = n_schannels
+        self.n_features = (n_channels + n_schannels)*4
+
+    def _init_ca_layers( self  ):
+        if self.n_schannels > 0:
+            self.combined_inp = Concatenate(axis=-1)
+
+        self.perception = DepthwiseConv2D(\
+            kernel_size=3,\
+                padding='valid',\
+                    depth_multiplier= 4,\
+                            depthwise_initializer= ImgCA.PerceptionKernelInitializer())
+
+        self.perception.trainable = False
+
+        self.features =  Conv2D(filters=self.n_features,\
+                kernel_size=1,\
+                bias_initializer='glorot_uniform',\
+                activation = 'relu') 
+
+        self.new_state = Conv2D(filters=self.n_channels + self.n_schannels,\
+            kernel_size=1,\
+                use_bias=False,\
+                kernel_initializer=tf.keras.initializers.Zeros())
+
+
+
+    def _circular_pad( self, x, pad  ):
+        x = tf.concat([x[:, -pad:], x, x[:, :pad]], axis=1)
+        x = tf.concat([x[:, :, -pad:], x, x[:, :, :pad]], axis=2)
+        return x
+
+
+    def _mask_signal_channels( self, x, split_sizes ):
+        b, h, w, c = x.shape
+        signal_mask = tf.zeros((b,h,w,ImgCA.n_schannels)) 
+        features, _  = tf.split(x, split_sizes, axis=-1 )
+        x=tf.concat([features, signal_mask], axis=-1)
+        return x
+
+    def call( self, x, s=None, update_rate=0.5, training_type='leaf'  ):
+        
+        b,h,w,c = x.shape
+        udpate_mask = tf.floor(tf.random.uniform(shape=(b,h,w,1) )+update_rate)
+        
+        if s is not None:
+            x = self.combined_inp([x,s])
+
+        x_pad = self._circular_pad(x,1)
+        y = self.perception(x_pad)
+        y = self.features(y)
+        y = self.new_state(y)
+        # TODO: is the below statement even required? Will it not make the learning error prone?
+        #if training_type=='leaf': y = self._mask_signal_channels(y, [self.n_channels,-1 ])
+        y = y*udpate_mask + x
+               
+        return y
+
+    def step( self, x, s=None, n_steps = 50, update_rate=0.5, training_type='leaf' ):
+
+        for _ in tf.range(n_steps):
+            x = self(x, s, update_rate,training_type )
+        return x
+    
+    # TODO: review this.
+    def make_seed(self, size, n=1):
+        x = np.ones([n, size, size, self.n_channels ], np.float32)
+        s = np.zeros([n, size, size, self.n_schannels ], np.float32)
+        ca_seed = np.concatenate([x,s], axis=-1)
+        return ca_seed
+
+    
+  
---
+++ models/HCAImgModel.py
@@ -0,0 +1,226 @@
+from hnca.framework.ca import ImgCA
+from hnca.framework.losses import StyleLoss, MSELoss
+from hnca.framework.utils import load_image, show_image, plot_loss
+from hnca.framework.types import ReplayBuffer
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+from keras import Model
+from keras.layers import UpSampling2D,Conv2D,AveragePooling2D
+from tqdm import tqdm
+
+
+
+"""
+This class uses the HNCA framework to create a hierachical CA model
+   
+    
+Attributes
+----------
+    
+Methods
+-------
+
+"""
+
+class HCAImgModel(Model):
+
+    def __init__( self,\
+                 leaf_ca_target,parent_ca_target,\
+                      leaf_ca_min_steps=32, leaf_ca_max_steps=96,  parent_ca_min_steps=32, parent_ca_max_steps=96,\
+                            leaf_ca_loss_type='ot', parent_ca_loss_type='mse' ):
+
+        super(HCAImgModel,self).__init__()
+
+        self._init_ca_class_members(self,leaf_ca_target,parent_ca_target,\
+                      leaf_ca_min_steps, leaf_ca_max_steps,  parent_ca_min_steps, parent_ca_max_steps)
+
+        self._init_loss_objects(leaf_ca_loss_type, parent_ca_loss_type )
+
+        self._init_ca_layers()
+        
+        
+
+                      
+    def _init_ca_class_members(self,leaf_ca_target,parent_ca_target,\
+                      leaf_ca_min_steps, leaf_ca_max_steps, parent_ca_min_steps, parent_ca_max_steps):
+                            
+
+        self.leaf_ca_model =  ImgCA(n_channels=12,n_schannels=4 )
+        self.parent_ca_model =  ImgCA(n_channels=12,n_schannels=0 )
+        self.leaf_ca_min_steps = leaf_ca_min_steps
+        self.leaf_ca_max_steps = leaf_ca_max_steps
+        self.parent_ca_min_steps = parent_ca_min_steps
+        self.parent_ca_max_steps = parent_ca_max_steps
+        self.signaling_factor = 4
+        self.leaf_img_target_size = 128
+        self.parent_img_target_size = 32
+        self.leaf_ca_target_img = load_image(leaf_ca_target)[None,:,:,:3]
+        self.parent_ca_target_img = load_image(parent_ca_target)[None,:,:,:3]
+
+        self.leaf_replay_buffer = ReplayBuffer()
+        self.leaf_replay_buffer.add( self.ca_leaf.make_seed(size=self.leaf_img_target_size, n=256))
+
+
+    def _init_loss_objects(self, leaf_ca_loss_type, parent_ca_loss_type ):
+
+        if leaf_ca_loss_type in ['gram','ot']:
+            self.leaf_ca_loss = StyleLoss( np.copy(self.target_img), leaf_ca_loss_type )
+        elif leaf_ca_loss_type == 'mse':
+            self.leaf_ca_loss = MSELoss()
+        else :
+            print("Leaf CA Loss type not supported")
+        
+        if parent_ca_loss_type in ['gram','ot']:
+            self.parent_ca_loss = StyleLoss( np.copy(self.parent_ca_target_img), parent_ca_loss_type )
+        elif parent_ca_loss_type == 'mse':
+            self.parent_ca_loss = MSELoss()
+        else :
+            print("Parent CA Loss type not supported")
+
+
+    def _init_ca_layers( self  ):
+
+        self.signal_creator =  Conv2D(filters=4,\
+                                        kernel_size=1,\
+                                            bias_initializer='glorot_uniform',\
+                                                kernel_initializer=tf.keras.initializers.Zeros()) # use linear activation
+                                                                                                  # should we use_bias?
+        self.feedback = AveragePooling2D(pool_size=(self.signaling_factor,self.signaling_factor), strides= self.signaling_factor )
+        self.upscale_signal = UpSampling2D(size=(self.signaling_factor,self.signaling_factor))
+
+        initializer = tf.random_uniform_initializer(shape=[1], minval=-1.0 , maxval=1.0, type=tf.float32)
+        self.signal_lr = tf.Variable(initializer, trainable=True)
+
+
+    def _get_signal(self, x  ):
+            
+        split_sizes = [3, -1] # remove latent channels from RGB channels 
+        _, s  = tf.split(x, split_sizes, axis=-1 )
+        s = self.signal_creator(s)
+        s = self.upscale_signal(s)
+        return s
+
+
+    def call(self, leaf_x, parent_x=None ):
+
+        step_n = np.random.randint(self.leaf_ca_min_steps, self.leaf_ca_max_steps)
+        if parent_x is None:
+            leaf_x = tf.stop_gradient(self.leaf_ca_model.step(leaf_x,s=None,n_steps=step_n, training_type='hca'))
+            parent_x = self.feedback(leaf_x)
+            parent_x = self.parent_ca_model(parent_x)
+
+        #Get signal from the parent CA
+        s = self._get_signal(parent_x)
+
+        #  get leaf CA features and signals
+        features, orig_signal = tf.split(leaf_x, [self.leaf_ca_model.n_channels, -1], axis=3 )
+
+        # add signal from parent CA to signal channels of leaf CA
+        s += orig_signal* self.signal_lr
+
+        # iterate through all n steps of leaf CA
+        leaf_x = self.leaf_ca_model.step(features, s=s, n_steps=step_n, training_type='hca')
+
+        # report pooled leaf CA channels back to the parent
+        parent_x = self.feedback(leaf_x)
+
+        parent_x = self.parent_ca_model(parent_x)
+
+        return leaf_x, parent_x 
+
+         
+    def pretrain_leaf_ca( self, lr=1e-3, num_epochs= 5000, use_pool=True, batch_size=4):
+
+        lr_sched = tf.keras.optimizers.schedules.PiecewiseConstantDecay([1000,2000], [lr, lr*0.3, lr*0.3*0.3])
+        optimizer = tf.keras.optimizers.Adam(lr_sched, epsilon=1e-08)
+        history = []
+        for e in tqdm(range(num_epochs)):
+            loss, tape = self._loss_step_leaf_ca(e, use_pool, batch_size)
+            variables = self.leaf_ca_model.trainable_variables
+            grads = tape.gradient(loss, variables)
+            grads = [g/(tf.norm(g)+1e-8) for g in grads]
+            optimizer.apply_gradients(zip(grads, variables))
+            history.append(loss.numpy())
+        
+        return history
+
+    def _loss_step_leaf_ca(self, curr_epoch, use_pool, batch_size):
+
+        if use_pool:
+            x = self.replay_buffer.sample_batch(batch_size)
+            if curr_epoch % 8 == 0:
+              x[:1]= self.leaf_ca_model.make_seed(self.leaf_img_target_size)
+        else:
+            x = self.leaf_ca_model.make_seed(self.leaf_img_target_size)
+
+        step_n = np.random.randint(self.leaf_ca_min_steps, self.leaf_ca_max_steps)
+        with tf.GradientTape() as t:
+            x = self.leaf_ca_model.step(x, s=None, n_steps=step_n, training_type='leaf_ca')
+            #overflow loss forces the model to output values within -1.0 and 1.0
+            overflow_loss = tf.reduce_sum(tf.abs(x - tf.clip_by_value(x, -1.0, 1.0)))
+            loss = self.leaf_ca_loss(tf.identity(x)) # + overflow_loss*1e2
+
+        if use_pool :
+          self.leaf_replay_buffer.add(x.numpy())
+
+        return loss, t
+
+    
+    def _loss_step_hca(self, curr_epoch, use_pool, batch_size):
+        
+        if use_pool:
+            leaf_x = self.replay_buffer.sample_batch(batch_size)
+            if curr_epoch % 8 == 0:
+              leaf_x[:1]= self.leaf_ca_model.make_seed(self.leaf_img_target_size)
+        else:
+            leaf_x = self.leaf_ca_model.make_seed(self.leaf_img_target_size)
+
+        step_n = np.random.randint(self.parent_ca_min_steps, self.parent_ca_max_steps)
+        with tf.GradientTape() as t:
+            leaf_x, parent_x = self(leaf_x, None )
+            for _ in tf.range(step_n):
+                leaf_x, parent_x = self(leaf_x, parent_x)
+            loss = self.parent_ca_loss(self.target_img, parent_x, is_image=True )
+
+        if use_pool :
+          self.leaf_replay_buffer.add(leaf_x.numpy())
+        return loss, t
+
+
+    def train_hca( self, lr=1e-3, num_epochs= 5000, use_pool=True, batch_size=4 ):
+        optimizer_leaf_ca = tf.keras.optimizers.Adam(learning_rate=lr)
+        optimizer_parent_ca = tf.keras.optimizers.Adam(learning_rate=lr)
+        parent_ca_history = []
+        leaf_ca_history = []
+        #TODO lr-patience and learning patience
+        #TODO frequency of training leaf ca
+        for e in tqdm(tf.range(num_epochs)):
+
+            loss_parent_ca,hca_tape = self._loss_step_hca( e, use_pool, batch_size )
+            parent_ca_history.append(loss_parent_ca.numpy())
+            gradients_parent_ca = hca_tape.gradient(loss_parent_ca, self.trainable_variables) 
+            optimizer_parent_ca.apply_gradients(zip(gradients_parent_ca, self.trainable_variables))
+
+            loss_leaf_ca,leaf_ca_tape = self._loss_step_leaf_ca( e,use_pool, batch_size, training_type='leaf_ca' )
+            leaf_ca_history.append(loss_leaf_ca.numpy())
+            gradients_leaf_ca = leaf_ca_tape.gradient(loss_leaf_ca, self.leaf_ca_model.trainable_variables)
+            optimizer_leaf_ca.apply_gradients(zip(gradients_leaf_ca, self.leaf_ca_model.trainable_variables))
+           
+
+    def create( self, x_initial = None, num_steps=50 ):
+        x = x_initial
+        
+        if x is None:
+          leaf_x = self.leaf_ca_model.make_seed(self.target_size, n=1)
+
+        leaf_x, parent_x = self(leaf_x,None)
+        for _ in tf.range(num_steps-1):
+            leaf_x, parent_x = self(leaf_x, parent_x, use_seed=False)
+
+        return leaf_x
+    
+
+    
+
+    
