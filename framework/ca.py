f981c7d7975f01b43231cc4ab564d72adcde0589
Ritu Pande <ritz.pande@gmail.com>
2022-11-21 19:14:52 +0000

    re-introducing batch normalization

--- framework/ca.py
+++ framework/ca.py
@@ -57,6 +57,7 @@ class ImgCA(Model):
         if self.n_schannels > 0:
             self.combined_inp = Concatenate(axis=-1)
 
+        self.bn0 =  BatchNormalization()
         self.perception = DepthwiseConv2D(\
             kernel_size=3,\
                 padding='valid',\
@@ -64,12 +65,13 @@ class ImgCA(Model):
                             depthwise_initializer= ImgCA.PerceptionKernelInitializer(self))
 
         self.perception.trainable = False
-
+        self.bn1 =  BatchNormalization()
         self.features =  Conv2D(filters=self.n_features,\
                 kernel_size=1,\
                 bias_initializer='glorot_uniform',\
                 activation = 'relu') 
 
+        self.bn2 =  BatchNormalization()
         
         self.new_state = Conv2D(filters=self.n_channels + self.n_schannels,\
             kernel_size=1,\
@@ -105,8 +107,11 @@ class ImgCA(Model):
             x = self.combined_inp([x,s])
 
         x_pad = self._circular_pad(x,1)
-        y = self.perception(x_pad)
+        y = self.bn0(x_pad)
+        y = self.perception(y)
+        y = self.bn1(y)
         y = self.features(y)
+        y = self.bn2(y)
         y = self.new_state(y)
         
         y = y*udpate_mask + x
