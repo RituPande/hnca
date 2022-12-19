30224cf995994d42ad87f959e164630b443650cf
Ritu Pande <ritz.pande@gmail.com>
2022-12-19 04:27:41 +0000

    creating dedicated comm layer

--- framework/comm.py
+++ framework/comm.py
@@ -1,42 +1,72 @@
 
+from numpy import nonzero
 import tensorflow as tf
 from tensorflow import keras
 from keras import Model
 from keras.layers import Conv2D,AveragePooling2D,UpSampling2D
 
 class CAComm(Model):
-    def __init__(self, n_leaf_ca_schannels, signal_factor  ):
+    def __init__(self, n_leaf_ca_channels, n_leaf_ca_schannels, signal_factor,\
+                   use_all_ch_in_signal_src, use_all_ch_in_signal_dst ):
+
         super(CAComm, self).__init__()
-        self.signal_creator =  Conv2D(filters=n_leaf_ca_schannels,\
+
+        self.n_leaf_ca_channels = n_leaf_ca_channels
+        self.n_leaf_ca_schannels = n_leaf_ca_schannels
+        self.signal_factor = signal_factor
+        self.use_all_ch_in_signal_src = use_all_ch_in_signal_src
+        self.use_all_ch_in_signal_dst = use_all_ch_in_signal_dst
+
+        n_filters = (n_leaf_ca_channels + n_leaf_ca_schannels) \
+                                if use_all_ch_in_signal_dst else n_leaf_ca_schannels
+
+        self.signal_creator =  Conv2D(filters=n_filters,\
                                         kernel_size=1,\
                                             bias_initializer='glorot_uniform',\
                                                 kernel_initializer=tf.keras.initializers.Zeros()) # use linear activation
                                                                                                   # should we use_bias?
-        self.feedback = AveragePooling2D(pool_size=(self.signaling_factor,self.signaling_factor), strides= self.signaling_factor )
-        self.upscale_signal = UpSampling2D(size=(self.signaling_factor,self.signaling_factor))
-
-        initializer = tf.random_uniform_initializer(minval=-1.0 , maxval=1.0,  )
+        self.feedback = AveragePooling2D(pool_size=(signal_factor,signal_factor), strides=signal_factor )
+        self.upscale_signal = UpSampling2D(size=(signal_factor,signal_factor))
+        initializer = tf.random_uniform_initializer(minval=-1.0 , maxval=1.0  )
         self.signal_lr = tf.Variable( initializer(shape=[1], dtype=tf.float32),  trainable=True)
 
-    def call(self, parent_x, leaf_x, use_rgb_in_signal_src=False, use_rgb_in_signal_dst=False ):
 
-      s = parent_x
-      if not use_rgb_in_signal_src:
+    def _create_signal(self, x):
+
+      s = x
+      if not self.use_all_ch_in_signal_src:
         split_sizes = [3, -1] # remove latent channels from RGB channels 
-        _, s  = tf.split(parent_x, split_sizes, axis=-1 )
+        _, s  = tf.split(x, split_sizes, axis=-1 )
             
       s = self.signal_creator(s)
-      
       s = self.upscale_signal(s)
 
+      return s
+
+    def _mix_signal( self, leaf_x, s):
+
       orig_signal = leaf_x
-      if not use_rgb_in_signal_dst:
-        _ , orig_signal = tf.split(leaf_x, [self.leaf_ca_model.n_channels, -1], axis=3 )
+      if not self.use_all_ch_in_signal_dst:
+        orig_ch , orig_signal = tf.split(leaf_x, [self.leaf_ca_model.n_channels, -1], axis=-1 )
 
       # add signal from parent CA to signal channels of leaf CA
-      new_leaf_x = orig_signal + self.signal_lr*s
+      new_signal_ch = orig_signal + self.signal_lr*s
 
-      return new_leaf_x
+      out = (orig_ch,new_signal_ch)  if not self.use_all_ch_in_signal_dst else (new_signal_ch, None)
+
+      return out
+
+
+
+    def call(self, parent_x, leaf_x, comm_type='signal' ):
+
+      if comm_type =='signal':
+        s = self._create_signal(parent_x)
+        out = self._mix_signal(leaf_x, s)
+      elif comm_type == 'feedback':
+        out = self.feedback(leaf_x)
+
+      return out
 
 
 
