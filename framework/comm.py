79014acf2cba0128d1a047dd23d0931326610ae5
Ritu Pande <ritz.pande@gmail.com>
2022-12-26 01:40:58 +0000

    create separate sensor and actuator classes

--- framework/comm.py
+++ framework/comm.py
@@ -1,9 +1,103 @@
-
-from numpy import nonzero
 import tensorflow as tf
 from tensorflow import keras
 from keras import Model
 from keras.layers import Conv2D,AveragePooling2D,UpSampling2D, Dense
+from hnca.framework.multiplexer import SimpleMultiplexer,CrossAttMultiplexer 
+
+class Sensor(Model):
+
+  def __init__( self, n_leaf_ca_channels, n_parent_ca_channels,\
+                   signal_factor,\
+                       n_sig_creation_layers,\
+                        all_ch_in_src,\
+                          all_ch_in_dst,\
+                            multiplex_type='simple',\
+                            ):
+    super(Sensor,self)
+    self.n_leaf_ca_channels = n_leaf_ca_channels
+    self.all_ch_in_src = all_ch_in_src
+    self.all_ch_in_dst = all_ch_in_dst # not used currently. Always set to True
+    self.signal_factor = signal_factor
+
+    self.feedback = AveragePooling2D(pool_size=(self.signal_factor,self.signal_factor), strides=self.signal_factor )
+    self.signal_creator =  [Conv2D(filters=n_parent_ca_channels,\
+                                        kernel_size=1,\
+                                            bias_initializer='glorot_uniform',\
+                                                kernel_initializer=tf.keras.initializers.Zeros()) \
+                                                  for _ in tf.range( self.n_sig_creation_layers ) ]
+
+    if multiplex_type == 'simple':
+      self.multiplexer = SimpleMultiplexer( n_features=n_parent_ca_channels)
+
+    elif multiplex_type == 'cross_attention':
+      self.multiplexer = CrossAttMultiplexer(d=16)
+
+
+  def call( self, x_src, x_dst ):
+
+    x_src = x_src if self.all_ch_in_src else tf.split(x_src, [self.n_leaf_ca_channels, -1 ], axis=-1)[1]
+    s = self.feedback(x_src)
+    for i in tf.range(self.n_sig_creation_layers):      
+        s = self.signal_creator[i](s)
+
+    out = self.multiplexer(x_dst, s)
+    return out
+    
+    
+
+class Actuator(Model):
+
+  def __init__( self, n_leaf_ca_channels, n_leaf_ca_schannels, 
+                        signal_factor,\
+                          n_sig_creation_layers,\
+                            all_ch_in_src, all_ch_in_dst,\
+                              multiplex_type='simple' ):
+    super(Actuator,self )
+
+    self.all_ch_in_src = all_ch_in_src
+    self.all_ch_in_dst = all_ch_in_dst
+    self.signal_factor = signal_factor
+    self.n_sig_creation_layers = n_sig_creation_layers
+
+    n_actuation_filters = (n_leaf_ca_channels + n_leaf_ca_schannels) \
+                                if self.all_ch_dst else n_leaf_ca_schannels
+
+    self.signal_creator =  [Conv2D(filters=n_actuation_filters,\
+                                        kernel_size=1,\
+                                            bias_initializer='glorot_uniform',\
+                                                kernel_initializer=tf.keras.initializers.Zeros()) \
+                                                  for _ in tf.range( self.n_sig_creation_layers ) ]
+
+
+    if multiplex_type == 'simple':
+      n = (n_leaf_ca_channels + n_leaf_ca_schannels) \
+                                if self.all_ch_dst else n_leaf_ca_schannels
+
+      self.multiplexer = SimpleMultiplexer( n_features=n)
+
+    elif multiplex_type == 'cross_attention':
+      self.multiplexer = CrossAttMultiplexer(d=16)
+
+
+  
+  def call(self, x_src, x_dst ):
+    
+    # create signal from source
+    s = x_src if self.all_ch_src else tf.split(x_src, [3,-1], axis=-1 )[1] # remove rgb channels
+    for i in tf.range(self.n_actuator_sig_creation_layers):      
+        s = self.signal_creator[i](s)
+    s = self.upscale_signal(s)
+
+    # multiplex to destination
+    x_dst_feat_ch, x_dst_latent_ch = tf.split(x_src, [self.n_leaf_ca_channels,-1], axis=-1 )
+    if self.all_ch_in_dst: 
+        x_dst_sig_ch = x_dst
+    else:
+        x_dst_feat_ch, x_dst_sig_ch = tf.split(x_src, [self.n_leaf_ca_channels,-1], axis=-1 )
+        
+    mixed_s_ch = self.multiplexer(x_dst_sig_ch, s)
+    out = (mixed_s_ch, None) if self.all_ch_in_dst else (x_dst_feat_ch, mixed_s_ch )
+    return out
 
 class CAComm(Model):
     def __init__(self, n_leaf_ca_channels, n_leaf_ca_schannels, n_parent_ca_channels, config_params ):
