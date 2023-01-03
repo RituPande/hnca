8c95b6107624481b6113cc45b477d83ecb0bbb2d
Ritu Pande <ritz.pande@gmail.com>
2023-01-03 17:13:32 +0000

    take seed as an argument for pre-train leaf_ca function

--- models/hncaimgmodel.py
+++ models/hncaimgmodel.py
@@ -79,8 +79,7 @@ class HCAImgModel(Model):
 
         self.leaf_replay_buffer = ReplayBuffer()
         self.parent_replay_buffer = ReplayBuffer(max_len=20000)
-        self.leaf_replay_buffer.add( self.leaf_ca_model.make_seed(size=self.leaf_img_target_size, n=256))
-
+        
         self._init_loss_objects(leaf_ca_loss_type, parent_ca_loss_type )
 
         
@@ -147,8 +146,8 @@ class HCAImgModel(Model):
 
         return leaf_x, parent_x 
     """
-         
-    def pretrain_leaf_ca( self, lr=1e-3, num_epochs= 5000, use_pool=True, batch_size=4):
+    """  
+      def pretrain_leaf_ca( self, lr=1e-3, num_epochs= 5000, use_pool=True, batch_size=4):
 
         lr_sched = tf.keras.optimizers.schedules.PiecewiseConstantDecay([1000,2000], [lr, lr*0.3, lr*0.3*0.3])
         optimizer = tf.keras.optimizers.Adam(lr_sched, epsilon=1e-08)
@@ -170,7 +169,12 @@ class HCAImgModel(Model):
         return history
 
     """
-      def pretrain_leaf_ca( self, lr=1e-3, num_epochs= 5000, use_pool=True, batch_size=4, num_batches_per_epoch=8, es_patience_cfg=1000, lr_patience_cfg=500, reseed_freq=1):
+    def pretrain_leaf_ca( self, seed_args, num_epochs= 5000, lr=1e-3, use_pool=True, batch_size=4, num_batches_per_epoch=8, es_patience_cfg=1000, lr_patience_cfg=500):
+
+        seed = seed_args['seed']
+        repeat_count = self.leaf_replay_buffer.maxlen
+        repeated_seeds = np.repeat(seed, repeat_count, axis=0)
+        self.leaf_replay_buffer.add(repeated_seeds)
 
         lr_sched = tf.keras.optimizers.schedules.PiecewiseConstantDecay([1000,2000], [lr, lr*0.3, lr*0.3*0.3])
         optimizer = tf.keras.optimizers.Adam(lr_sched, epsilon=1e-08)
@@ -184,7 +188,7 @@ class HCAImgModel(Model):
         for e in tqdm(range(num_epochs)):
             batch_loss = 0
             for b in tf.range(num_batches_per_epoch):
-              loss, tape = self._loss_step_leaf_ca(e, use_pool, batch_size, reseed_freq)
+              loss, tape = self._loss_step_leaf_ca(e, use_pool, batch_size, seed_args)
               batch_loss += loss
               variables = self.leaf_ca_model.trainable_variables
               grads = tape.gradient(loss, variables)
@@ -216,7 +220,7 @@ class HCAImgModel(Model):
         
         return history
 
-    """
+  
 
     def pretrain_parent_ca(self, seed_args, start_epoch=0, lr=1e-3, num_epochs= 5000,\
                                    use_pool=True, batch_size=4,\
@@ -282,16 +286,20 @@ class HCAImgModel(Model):
                         
       return history
 
-    def _loss_step_leaf_ca(self, curr_epoch, use_pool, batch_size):
+    def _loss_step_leaf_ca(self, curr_epoch, use_pool, batch_size, seed_args):
 
         if use_pool:
             x = self.leaf_replay_buffer.sample_batch(batch_size)
-            if curr_epoch % 8 == 0:
-              x[:1]= self.leaf_ca_model.make_seed(self.leaf_img_target_size)
+            if curr_epoch % seed_args['reseed_freq'] == 0:
+              x[0] = seed_args['seed'][0]
+              #x[:1]= self.leaf_ca_model.make_seed(self.leaf_img_target_size)
         else:
             x = self.leaf_ca_model.make_seed(self.leaf_img_target_size)
 
-        x,s = tf.split(x,[self.leaf_ca_model.n_channels,-1], axis=-1)
+        if self.leaf_ca_model.n_schannels:
+          x,s = tf.split(x,[self.leaf_ca_model.n_channels,-1], axis=-1)
+        else:
+          s = None
         step_n = np.random.randint(self.leaf_ca_min_steps, self.leaf_ca_max_steps)
         with tf.GradientTape() as t:
             x = self.leaf_ca_model.step(x, s, n_steps=step_n, training_type='leaf')
